3c437279e5ca82025d7da6ab008af5cd456516e8
yvsc <108088938+yvsc@users.noreply.github.com>
2022-08-11 14:06:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -174,7 +174,7 @@ var addMarkers = function(data) {
   }
 
   L.control.layers({}, groups, {collapsed: false}).addTo(map);
-  $('.leaflet-control-layers-overlays').prepend('<h3 class="mt0 mb1 f5 black-30">Themes</h3>');
+  $('.leaflet-control-layers-overlays').prepend('<h3 class="mt0 mb1 f5 black-30"> Projects </h3>');
 
   // If name in hash, activate it
   if (activeMarker) { activeMarker.fire('click') }
